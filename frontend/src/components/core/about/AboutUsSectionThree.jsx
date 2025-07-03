70bf6b8e58911727cd0fadd6b43ec496196af12a
atharvakasture2004 <atharvakasture13@gmail.com>
2025-07-03 18:16:35 +0530

    fix: mobile view of about us

--- frontend/src/components/core/about/AboutUsSectionThree.jsx
+++ frontend/src/components/core/about/AboutUsSectionThree.jsx
@@ -1,39 +1,41 @@
-import CoreCard from "../about/cards/CoreCard";
+import CoreCard from '../about/cards/CoreCard';
 
 function AboutUsSectionThree() {
   const data = [
     {
-      title: "Our Mission",
+      title: 'Our Mission',
       content:
-        "we have many songs and stories to tell just be with us we have many songs and stories to tell ",
+        'we have many songs and stories to tell just be with us we have many songs and stories to tell ',
     },
     {
-      title: "Our Vision",
+      title: 'Our Vision',
       content:
-        "we have many songs and stories to tell just be with us we have many songs and stories to tell ",
+        'we have many songs and stories to tell just be with us we have many songs and stories to tell ',
     },
     {
-      title: "Our Value",
+      title: 'Our Value',
       content:
-        "we have many songs and stories to tell just be with us we have many songs and stories to tell ",
+        'we have many songs and stories to tell just be with us we have many songs and stories to tell ',
     },
   ];
 
   return (
-    <div className="w-full h-[820px] bg-white font-['Roboto'] flex justify-center px-4 py-12">
-      <div className="w-[1280px] flex flex-col items-center">
+    <div className="w-full h-[1300px] pb-20 md:h-[832px] bg-white font-roboto flex justify-center">
+      <div className='w-[1280px] flex flex-col items-center gap-[40px]'>
         {/* Center Title + Subtitle */}
-        <div className="w-[792px] h-[157px] flex flex-col gap-[13px] items-center text-center mb-14 mt-20 md:mt-40">
-          <h1 className="w-[792px] h-[60px] text-[32px] md:text-[51.2px] font-medium leading-[100%] tracking-normal text-black">
+        <div className='w-[792px] h-[157px] flex-col gap-[13px] items-center text-center mb-14 mt-20 md:mt-40 hidden sm:block'>
+          <h1 className='w-[792px] h-[60px] text-[32px] md:text-[51.2px] font-medium leading-[100%] tracking-normal text-black'>
             Our Mission, Vision and Values
           </h1>
-          <p className="w-[792px] h-[84px] text-[18px] md:text-[24px] font-normal leading-[100%] text-black">
-            we have many songs and stories to tell just be with us we have many songs and stories to tell just be with us we have many songs and stories to tell just be with us
+          <p className='w-[792px] h-[84px] text-[18px] md:text-[24px] font-normal leading-[100%] text-black'>
+            we have many songs and stories to tell just be with us we have many
+            songs and stories to tell just be with us we have many songs and
+            stories to tell just be with us
           </p>
         </div>
 
         {/* Cards Section */}
-        <div className="flex flex-wrap justify-center gap-[60px] w-[930px] h-[360px]">
+        <div className='flex flex-wrap justify-center gap-[58px] sm:gap-[60px] sm:w-[930px] sm:h-[360px] w-[270px] h-[1200px]'>
           <CoreCard data={data} />
         </div>
       </div>
--- frontend/src/components/core/about/cards/CarouselSideCard.jsx
+++ frontend/src/components/core/about/cards/CarouselSideCard.jsx
@@ -1,17 +1,16 @@
-// import pattern from "../../../assets/pattern.svg"
 import pattern from "../../../../assets/pattern.svg";
 
 function CarouselSideCard({ data }) {
   return (
-    <div className="w-[300px] h-[200px] bg-gradient-to-b from-[#e6f7f7] to-[#ffffff]  border border-black shadow-md rounded-[6px] flex flex-col justify-center pl-8 relative">
-      <div className="text-[20px] font-semibold text-black z-10 relative">
+    <div className="w-[237px] h-[200px] bg-gradient-to-b from-[#e6f7f7] to-[#ffffff] border border-black shadow-md rounded-[6px] flex flex-col justify-center items-center p-4">
+      <div className="text-[20px] font-semibold text-black z-10 relative text-center">
         {data.title}
       </div>
       {/* Background pattern */}
       <img
         src={pattern}
         alt="pattern"
-        className="absolute bottom-0 left-0 w-full object-cover h-[70px] z-0"
+        className="absolute bottom-0 left-0 w-full object-cover h-[90px] z-0"
       />
     </div>
   );
